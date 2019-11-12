12186ae257644f419d29654e52d676b96b52110f
unknown <eneko.atxa@opendeusto.es>
2019-11-12 14:28:04 +0100

    Second commit

--- appBookStore/models.py
+++ appBookStore/models.py
@@ -1,31 +1,22 @@
 from django.db import models
 
+
+class Autor(models.Model):
+   nombre = models.CharField(max_length=40)
+   apellido = models.CharField(max_length=40)
+   fecha_nacimiento = models.DateField()
+   foto = models.ImageField(upload_to='appBookStore/static/img',blank=True,null=True,verbose_name='Image')
+   
+class Editorial(models.Model):
+   nombre = models.CharField(max_length=40)
+
 class Libro(models.Model):
-   # No es necesario crear un campo para la Primary Key, Django creará automáticamente un IntegerField.
-  # cod_libro = models.IntegerField()
    titulo = models.CharField(max_length=50)
    num_paginas = models.IntegerField()
    genero = models.CharField(max_length=50)
-  # portada = models.Imagefield()
+   portada = models.ImageField(upload_to='appBookStore/static/img',blank=True,null=True,verbose_name='Image')
    anyo = models.IntegerField()
    edicion = models.IntegerField()
    idioma = models.CharField(max_length=50)
    cod_editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
-
-class Autor(models.Model):
-   # Campo para la relación one-to-many
-  # departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-   nombre = models.CharField(max_length=40)
-   apellido = models.CharField(max_length=40)
-   fecha_nacimiento = models.DateField()
-  # foto = models.Imagefield()
-
-class Editorial(models.Model):
-   # Campo para la relación one-to-many
-  # cod_editorial = models.IntegerField()
-   nombre = models.CharField(max_length=40)
-
-class Escribe(models.Model):
-   # Campo para la relación one-to-many
-   cod_libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
-   cod_autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+   autores = models.ManyToManyField(Autor)
--- bookStore/urls.py
+++ bookStore/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
-	path('', include(appBookStore.urls))
+	path('', include('appBookStore.urls')),
     path('admin/', admin.site.urls),
 ]
