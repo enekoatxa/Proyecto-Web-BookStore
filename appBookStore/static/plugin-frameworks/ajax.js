d5b5ffd6148c487c3f4ea42081a12e858635fd4e
unknown <eneko.atxa@opendeusto.es>
2020-01-08 15:10:29 +0100

    qtip berriro

--- appBookStore/static/plugin-frameworks/ajax.js
+++ appBookStore/static/plugin-frameworks/ajax.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-
-  $('#contenidoAjax').qtip(
-  {
-    var href = $(this).attr("href");
+$('a.contenidoAjax').each(function(){
+	var href = $(this).attr("href");
     href = href.replace("libros", "ajax");
+    $(this).qtip({
     content: {
       url: href,
       method: 'get'
-   }
+ 	}
   });
-
-});
+}); 
+}); 
